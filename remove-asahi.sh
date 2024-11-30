5794d88b0793c4b9ed45bc2332adcf71a283b538
David P. Rush <davidprush@gmail.com>
2024-11-30 13:24:18 -0500

    Commented out the diskutil actions

--- remove-asahi.sh
+++ remove-asahi.sh
@@ -20,7 +20,7 @@ grow_macos_system(){
         exit 0
     fi
     echo "Resizing $macos_volume"
-    diskutil apfs resizeContainer $macos_volume 0
+    #diskutil apfs resizeContainer $macos_volume 0
     echo "$macos_volume resized"
 }
 
@@ -70,7 +70,7 @@ delete_partition() {
     for part in $partitions; do
         if can_delete_partition $part; then
             echo "Deleting partition $part..."
-            diskutil eraseVolume free free $part
+            #diskutil eraseVolume free free $part
         else
             echo "Skipping deletion of this partition."
         fi
@@ -96,7 +96,7 @@ delete_apfs_uefi() {
             echo "$part skipped"
         else
             echo "Deleting the APFS UEFI partition: $part..."
-             diskutil apfs deleteContainer $part
+            #diskutil apfs deleteContainer $part
             echo "$part deleted"
         fi
     else
